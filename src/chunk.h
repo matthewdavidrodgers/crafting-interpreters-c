b2a4b912e6da3be991573fba76bcaee20943ec43
Matthew Rodgers <matthewdavidrodgers@gmail.com>
2021-01-13 20:49:45 -0500

    Up through basic scanning

---
+++ src/chunk.h
@@ -0,0 +1,31 @@
+#ifndef clox_chunk_h
+#define clox_chunk_h
+
+#include "common.h"
+#include "memory.h"
+#include "value.h"
+
+typedef enum {
+  OP_CONSTANT,
+  OP_NEGATE,
+  OP_ADD,
+  OP_SUBTRACT,
+  OP_MULTIPLY,
+  OP_DIVIDE,
+  OP_RETURN
+} OpCode;
+
+typedef struct {
+  int count;
+  int capacity;
+  uint8_t* code;
+  ValueArray constants;
+  int* lines;
+} Chunk;
+
+void initChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+void freeChunk(Chunk* chunk);
+int addConstant(Chunk* chunk, Value value);
+
+#endif
