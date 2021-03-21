ab68355360eaea2e1691ebd1713b55ec7ce8826c
Matthew Rodgers <matthewdavidrodgers@gmail.com>
2021-03-21 16:50:03 -0700

    Add implementation through superclasses

--- src/debug.c
+++ src/debug.c
@@ -73,6 +73,7 @@ int disassembleInstruction(Chunk* chunk, int offset) {
         case OP_EQUAL:          return simpleInstruction("OP_EQUAL", offset);
         case OP_GET_PROPERTY:   return constantInstruction("OP_GET_PROPERTY", chunk, offset);
         case OP_SET_PROPERTY:   return constantInstruction("OP_SET_PROPERTY", chunk, offset);
+        case OP_GET_SUPER:      return constantInstruction("OP_GET_SUPER", chunk, offset);
         case OP_GREATER:        return simpleInstruction("OP_GREATER", offset);
         case OP_LESS:           return simpleInstruction("OP_LESS", offset);
         case OP_NEGATE:         return simpleInstruction("OP_NEGATE", offset);
@@ -86,6 +87,7 @@ int disassembleInstruction(Chunk* chunk, int offset) {
         case OP_LOOP:           return jumpInstruction("OP_JUMP", -1, chunk, offset);
         case OP_CALL:           return byteInstruction("OP_CALL", chunk, offset);
         case OP_INVOKE:         return invokeInstruction("OP_INVOKE", chunk, offset);
+        case OP_SUPER_INVOKE:   return invokeInstruction("OP_SUPER_INVOKE", chunk, offset);
         case OP_CLOSURE: {
             offset++;
             uint8_t constant = chunk->code[offset++];
@@ -105,6 +107,7 @@ int disassembleInstruction(Chunk* chunk, int offset) {
         case OP_CLOSE_UPVALUE:  return simpleInstruction("OP_CLOSE_UPVALUE", offset);
         case OP_RETURN:         return simpleInstruction("OP_RETURN", offset);
         case OP_CLASS:          return constantInstruction("OP_CLASS", chunk, offset);
+        case OP_INHERIT:        return simpleInstruction("OP_INHERIT", offset);
         case OP_METHOD:         return constantInstruction("OP_METHOD", chunk, offset);
         default:
             printf("Unknown opcode %d\n", instruction);
